6af142bc6a75aba95737b5ccb93eb481d2d0b973
Richard <richardbsanches@outlook.com>
2026-04-23 16:19:57 -0300

    criando estrutura basica do projeto

--- index.js
+++
@@ -1,6 +0,0 @@
-const { somarNumerosPares } = require("./tarefa");
-
-const numeros = [1, 2, 3, 4, 5, 6];
-const resultado = somarNumerosPares(numeros);
-
-console.log(`Soma dos números pares: ${resultado}`);
