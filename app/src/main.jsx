88dede89197c220d26316347e5a5205d4fd3470d
Richard <richardbsanches@outlook.com>
2026-04-23 17:51:28 -0300

    estrutura basica do front criada

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* adicione uma rota aqui */}
+        <Route path="/" element={<div className='bg-red-500'>Hello, World!</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
